de80f2ef18910b9d8193cc690d38be61a5ca716a
tsepanx <77626319+tsepanx@users.noreply.github.com>
2021-08-08 14:41:31 +0300

    Youtube: channel videos fetching with token page

---
+++ utils.py
@@ -0,0 +1,27 @@
+import feedparser
+import enum
+
+class VideoField:
+    ID = "yt_videoid"
+    URL = "link"
+    TITLE = "title"
+
+class ApiField:
+    PAGE_TOKEN = "pageToken"
+    NEXT_PAGE_TOKEN = "nextPageToken"
+
+# def parse_feedparserdict(d: feedparser.FeedParserDict):
+
+
+def channel_videos_from_search(d: dict, instance_type):
+    items = d['items']
+
+    for i in range(len(items)):
+        videoId = 'videoId'
+        
+        if videoId in items[i]['id']:
+
+            video_id = items[i]['id'][videoId]
+            title = items[i]['snippet']['title']
+
+            yield instance_type(video_id, title)
