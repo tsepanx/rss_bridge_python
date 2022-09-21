34ce38ce7fbed5cc29602601aeb58f830727c928
tsepanx <77626319+tsepanx@users.noreply.github.com>
2022-09-21 18:26:05 +0300

    feat: tg api channel posts parser as an Iterator with __next__

--- tg_api.py
+++ tg_api.py
@@ -1,10 +1,13 @@
 import datetime
 import re
 from dataclasses import dataclass
-from typing import Iterable
+from functools import lru_cache
+from typing import Optional
 
 import bs4
 import requests
+
+BASE_URL = 'https://t.me'
 
 
 @dataclass
@@ -15,69 +18,97 @@ class TGApiPost:
     preview_link: str = None
     preview_link_pic: str = None
 
+def logged_get(url):
+    res = requests.get(url)
+    print(f'REQUEST: {url}')
+    return res
 
-def parse_channel_posts(url: str) -> Iterable[TGApiPost]:
-    req = requests.get(url)
-    soup = bs4.BeautifulSoup(req.text, "html.parser")
-    a = soup.find(
-        name='div',
-        attrs={
-            'class': 'tgme_channel_info_header_title'},
-        recursive=True)
-    b = a.findChild(
-        name='span'
-    )
+class TGChannelPosts:
+    q = list()
+    next_url: str = None
 
-    channel_name = b.contents[0]
-    print(channel_name)
+    def __init__(self, url: str):
+        self.url = url
 
-    # ---
+        self.next_url = self.fetch_next_posts_page(self.url)
 
-    posts_wrapper = soup.find(
-        name='section',
-        attrs={
-            'class': 'tgme_channel_history'
-        },
-        recursive=True
-    )
+    def fetch_channel_name(self):
+        req = logged_get(self.url)
+        soup = bs4.BeautifulSoup(req.text, "html.parser")
 
-    posts_list = posts_wrapper.findChildren(
-        name='div',
-        attrs={
-            'class': 'tgme_widget_message_wrap'
-        },
-        recursive=True
-    )
-
-    for post in list(reversed(posts_list))[:3]:
-        href_date_elem = post.findChild(
-            name='a',
-            attrs={
-                'class': 'tgme_widget_message_date'
-            }
+        # --- Parse channel title ---
+        channel_title_wrapper = soup.find(
+            name='div', attrs={
+                'class': 'tgme_channel_info_header_title'},
+            recursive=True
         )
-        post_date = href_date_elem.contents[0].get_attribute_list('datetime')[0]
-        post_date = datetime.datetime.fromisoformat(post_date)  # Convert to pythonic format
-        print(post_date)
+        channel_title_tag = channel_title_wrapper.findChild(name='span')
+        channel_name = channel_title_tag.contents[0]
+        print(channel_name)
 
-        post_href = href_date_elem.get_attribute_list('href')[0]
-        print(post_href)
+    @lru_cache
+    def fetch_next_posts_page(self, fetch_url: str) -> Optional[str]:
+        """
+        :param fetch_url: Link to fetch previous channel posts.
+        example: https://t.me/s/notboring_tech?before=2422
+
+        :return: Next fetch_url for fetching next page of posts
+        """
+        req = logged_get(fetch_url)
+        soup = bs4.BeautifulSoup(req.text, "html.parser")
+
+        # --- Get list of posts wrappers
+        posts_list = soup.findChildren(
+            name='div', attrs={'class': 'tgme_widget_message_wrap'},
+            recursive=True
+        )
+
+        self.q = list(reversed(posts_list))
+
+        # --- Next messages page href parsing
+        messages_more_tag = soup.find(
+            name='a', attrs={'class': 'tme_messages_more'},
+            recursive=True
+        )
+
+        if messages_more_tag.get('data-after'):
+            # raise StopIteration
+            return None  # We reached end of posts list
+
+        next_page_href = messages_more_tag.get('href')
+        next_page_link = f'{BASE_URL}{next_page_href}'
+        return next_page_link
+
+    @staticmethod
+    def html_tag_to_dataclass(post: bs4.element.Tag) -> Optional[TGApiPost]:
+        href_date_tag = post.findChild(name='a', attrs={'class': 'tgme_widget_message_date'})
+        post_date = href_date_tag.contents[0].get('datetime')
+        post_date = datetime.datetime.fromisoformat(post_date)  # Convert from string to pythonic format
+
+        post_href = href_date_tag.get('href')
 
         text_wrapper = post.findChild(name='div', attrs={'class': 'tgme_widget_message_text'})
+        if not text_wrapper:
+            return None
         text = text_wrapper.get_text('\n', strip=True)
-        print(f'text: "{text}"')
 
         link_preview_wrapper = post.findChild(name='a', attrs={'class': 'tgme_widget_message_link_preview'})
-        link_preview = link_preview_wrapper.get_attribute_list('href')
 
-        link_preview_img_elem = post.findChild(name='i', attrs={'class': 'link_preview_right_image'})
-        link_preview_img_elem_style = link_preview_img_elem.get_attribute_list('style')[0]
-        r = r"background-image:url\('(.*)'\)"
-        link_preview_img = re.findall(r, link_preview_img_elem_style)[0]
+        if link_preview_wrapper:  # There is a preview section
+            link_preview = link_preview_wrapper.get('href')
 
-        print()
+            link_preview_img_tag = post.findChild(name='i', attrs={'class': 'link_preview_right_image'})
+            if link_preview_img_tag is None:
+                link_preview_img_tag = post.findChild(name='i', attrs={'class': 'link_preview_image'})
 
-        dc = TGApiPost(
+            link_preview_img_tag_style = link_preview_img_tag.get('style')
+            r = r"background-image:url\('(.*)'\)"
+            link_preview_img = re.findall(r, link_preview_img_tag_style)[0]
+        else:
+            link_preview = None
+            link_preview_img = None
+
+        return TGApiPost(
             datetime=post_date,
             url=post_href,
             text=text,
@@ -85,9 +116,38 @@ def parse_channel_posts(url: str) -> Iterable[TGApiPost]:
             preview_link_pic=link_preview_img,
         )
 
-        # yield dc
-        # exit()
+    def __iter__(self):
+        return self
+
+    def __next__(self) -> TGApiPost:
+        if len(self.q) > 0:
+            next_post = self.q[0]
+            self.q.pop(0)
+
+            post_dataclass = self.html_tag_to_dataclass(next_post)
+            if post_dataclass:
+                return post_dataclass
+            else:
+                return self.__next__()
+
+        else:  # No left fetched posts
+            print(f'Fetching new posts page')
+            if self.next_url:
+                self.next_url = self.fetch_next_posts_page(self.next_url)
+                return self.__next__()
+            else:
+                raise StopIteration
 
 
 if __name__ == "__main__":
-    parse_channel_posts('https://t.me/s/prostyemisli')
+    gen = TGChannelPosts('https://t.me/s/prostyemisli')
+
+    # for i in range(200):
+    #     c = gen.__next__()
+    #     print(f'href: "{c.url}"')
+    #     print(f'text: "{c.text}"')
+    #     print(f'datetime: "{c.datetime}"')
+    #     print('\n\n')
+
+    for i in list(gen):
+        print(i.url)
